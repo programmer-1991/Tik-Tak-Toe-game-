38ea05f622093d470522b25f1f2e26f7a08c4171
programmer-1991 <>
2023-06-17 13:33:46 +0000

    create print_board function

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
 
 board = ["-", "-", "-",
-        "-", "-", "-",
-        "-", "-", "-"]
+         "-", "-", "-",
+         "-", "-", "-"]
 current_player = "X"
 winner = None
 game_is_running = True
+line = "-----"
+
+#printing the game board
+
+def print_board(board):
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print(line)
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(line)
+    print(board[6] + "|" + board[7] + "|" + board[8])
+
+print_board(board)
