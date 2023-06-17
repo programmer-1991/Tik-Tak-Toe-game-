bbe0c2a077b4add954ff20832d11bcf4e35a1a7d
programmer-1991 <>
2023-06-17 19:22:11 +0000

    add check functions

--- run.py
+++ run.py
@@ -19,7 +19,47 @@ def print_board(board):
 #take player input
 def player_input(board):
     your_input = int(input("choose a number between 1 and 9: "))
-    board[your_input-1] = current_player
+    if board[your_input-1] == "-":
+        board[your_input-1] = current_player
+    else:
+        print("the spot was already played!!")
+        
+#We check if we have three similar symbols in a row, horizontally, vertically, or diagonally.
+#we check horizontally: 3 possibilities
+def check_horizontally(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
+
+#we check vertically: 3 possibilities
+def check_vertically(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[3]
+        return True
+        
+#we check diagonally: 2 possibilities
+def check_diagonally(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[4] != "-":
+        winner = board[2]
+        return True
 
 
 while game_is_running:
